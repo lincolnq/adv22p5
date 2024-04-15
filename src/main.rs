94acc8ae0b777f68afce846e466ad6f446c11408
Lincoln Quirk <lincolnq@gmail.com>
2024-04-15 17:07:20 -0400

    solve part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::collections::HashMap;
+
+// import parse.rs
+mod parse;
+
 fn main() {
     println!("Hello, world!");
+
+    // we're going to consume stdin line by line
+    //let mut lines = std::io::stdin().lines();
+    let mut handle = std::io::stdin().lock();
+
+    // read seeds
+    let seeds = parse::parse_seeds(&mut handle);
+    println!("Parsed: {:?}", seeds);
+
+    // read map blocks
+    let mut map_blocks = Vec::new();
+    loop {
+        let map_block = parse::parse_map_block(&mut handle);
+        if map_block.is_none() {
+            break;
+        }
+        map_blocks.push(map_block.unwrap());
+    }
+    
+    // now run it on all the seeds
+    let mut results = HashMap::new();
+    for seed in seeds {
+        results.insert(seed, forward_pass_all(&map_blocks, seed));
+    }
+    println!("Results: {:?}", results);
+
+    // show the lowest result
+    let min = results.iter().min_by_key(|(_, &v)| v).unwrap();
+    println!("Lowest: {:?}", min);
+
+}
+
+// forward pass for a map block: map source to destination across all mappings in that block
+fn forward_pass(map_block: &parse::ParsedMapBlock, x: u64) -> u64 {
+    let mut matches = map_block.mappings.iter().filter(|(_, srcrs, len)| x >= *srcrs && x < *srcrs + *len);
+    if let Some((destrs, srcrs, _)) = matches.next() {
+        x - srcrs + destrs
+    } else {
+        x
+    }
+}
+
+// forward pass for the entire sequence: map seed to location
+fn forward_pass_all(map_blocks: &Vec<parse::ParsedMapBlock>, seed: u64) -> u64 {
+    map_blocks.iter().fold(seed, |acc, map_block| forward_pass(map_block, acc))
 }
---
+++ src/parse.rs
@@ -0,0 +1,66 @@
+use std::io::BufRead;
+
+// parse the seeds line from a filehandle
+// looks like this: 
+//    seeds: 79 14 55 13
+// always 1 line, but should consume 2 lines
+pub fn parse_seeds<R: BufRead>(reader: &mut R) -> Vec<u64> {
+    let mut seeds = Vec::new();
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+    let mut parts = line.split_whitespace();
+    assert_eq!(parts.next().unwrap(), "seeds:");
+    for part in parts {
+        seeds.push(part.parse().unwrap());
+    }
+    // consume the newline
+    reader.read_line(&mut line).unwrap();
+    seeds
+}
+
+// parse a map block from the filehandle
+// looks like this:
+//   seed-to-soil map:
+//   50 98 2
+//   52 50 48
+// ends in a blank line
+// we need the "seed-to-soil" string parsed into a tuple of (seed, soil)
+
+// first define a result datatype:
+#[derive(Debug)]
+pub struct ParsedMapBlock {
+    pub from: String,
+    pub to: String,
+    pub mappings: Vec<(u64, u64, u64)>,
+}
+
+// now the parser
+pub fn parse_map_block<R: BufRead>(reader: &mut R) -> Option<ParsedMapBlock> {
+    let mut line = String::new();
+    reader.read_line(&mut line).ok()?;
+    if line.trim().is_empty() {
+        return None;
+    }
+
+    let fromto = line.split_whitespace().next().unwrap().split("-to-").collect::<Vec<&str>>();
+    let from = String::from(fromto[0]);
+    let to = String::from(fromto[1]);
+
+    // ok, now let's read the mappings
+    let mut mappings = Vec::new();
+
+    loop {
+        line.clear();
+        reader.read_line(&mut line).unwrap();
+        if line.trim().is_empty() {
+            break;
+        }
+        let mut parts = line.split_whitespace();
+        let seed = parts.next().unwrap().parse().unwrap();
+        let soil = parts.next().unwrap().parse().unwrap();
+        let prob = parts.next().unwrap().parse().unwrap();
+        mappings.push((seed, soil, prob));
+    }
+
+    Some(ParsedMapBlock { from: from, to: to, mappings: mappings })
+}
